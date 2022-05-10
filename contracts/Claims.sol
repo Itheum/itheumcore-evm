c7c43f33b05a9d4f0fe0e6767e5098619ba14182
Peter Holzer <message@peterholzer.at>
2022-05-10 08:05:40 +0200

    added events

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -8,13 +8,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Claims is Ownable, Pausable, ReentrancyGuard {
 
+    event DepositIncreased(address indexed _address , uint8 indexed _type, uint _amount);
+    event DepositDecreased(address indexed _address , uint8 indexed _type, uint _amount);
+    event DepositClaimed(address indexed _address , uint8 indexed _type, uint _amount);
+
     struct Deposit {
         uint amount;
         uint lastDeposited;
     }
 
     uint public totalDeposits;
-    mapping(address => mapping(uint16 => Deposit)) public deposits;
+    mapping(address => mapping(uint8 => Deposit)) public deposits;
     ERC20 itheumTokenMYDA;
 
     constructor (address _itheumTokenMydaAddress) {
@@ -36,7 +40,7 @@ contract Claims is Ownable, Pausable, ReentrancyGuard {
 
     function increaseDeposit(
         address _address,
-        uint16 _type,
+        uint8 _type,
         uint _amount
     ) external onlyOwner whenNotPaused noAddressZero(_address) returns(bool success) {
         totalDeposits += _amount;
@@ -46,12 +50,14 @@ contract Claims is Ownable, Pausable, ReentrancyGuard {
         deposits[_address][_type].amount += _amount;
         deposits[_address][_type].lastDeposited = block.timestamp; // note that this can be influenced slightly by miners
 
+        emit DepositIncreased(_address, _type, _amount);
+
         return true;
     }
 
     function decreaseDeposit(
         address _address,
-        uint16 _type,
+        uint8 _type,
         uint _amount
     ) external onlyOwner whenNotPaused noAddressZero(_address) returns(bool success) {
         totalDeposits -= _amount;
@@ -59,10 +65,12 @@ contract Claims is Ownable, Pausable, ReentrancyGuard {
         deposits[_address][_type].amount -= _amount;
         deposits[_address][_type].lastDeposited = block.timestamp; // note that this can be influenced slightly by miners
 
+        emit DepositDecreased(_address, _type, _amount);
+
         return true;
     }
 
-    function claimDeposit(uint16 _type) external whenNotPaused nonReentrant returns(bool success) {
+    function claimDeposit(uint8 _type) external whenNotPaused nonReentrant returns(bool success) {
         require(deposits[msg.sender][_type].amount > 0, 'Nothing to claim');
 
         uint amount = deposits[msg.sender][_type].amount;
@@ -72,6 +80,8 @@ contract Claims is Ownable, Pausable, ReentrancyGuard {
         totalDeposits -= amount;
 
         itheumTokenMYDA.transferFrom(owner(), msg.sender, amount);
+
+        emit DepositClaimed(msg.sender, _type, amount);
 
         return true;
     }
