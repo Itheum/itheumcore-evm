589e1299c9e785b4b8aca333b6ee33cfb97bdd62
Peter Holzer <message@peterholzer.at>
2022-05-19 08:23:39 +0200

    minor improvements and style adaptions

--- contracts/dataNFT.sol
+++ contracts/dataNFT.sol
@@ -17,7 +17,6 @@ contract ItheumDataNFT is ERC721 {
     }
     
     struct DataNFT {
-        uint256 id; // todo I think it's unnecessary to store the NFT id here as well
         uint256 priceInItheum;
         address creator;
         uint8 royaltyInPercent;
@@ -25,73 +24,78 @@ contract ItheumDataNFT is ERC721 {
         string uri;
     }
     
-    mapping (uint256 => DataNFT) public dataNFTs; // todo implement public getter where we also check token id existence in _exists(tokenId)
+    mapping (uint256 => DataNFT) private _dataNFTs;
+
+    function dataNFTs(uint256 _tokenId) public view returns (DataNFT memory) {
+        require(_exists(_tokenId), "DataNFT doesn't exist");
+        return _dataNFTs[_tokenId];
+    }
     
-    function createDataNFT(string memory uri, uint256 priceInItheum, uint8 royaltyInPercent) public returns (uint256) {
-        require(priceInItheum > 0, "Price must be > 0");
-        require(royaltyInPercent <= 100, "Royalty must be <= 100");
+    function createDataNFT(string memory _uri, uint256 _priceInItheum, uint8 _royaltyInPercent) public returns (uint256) {
+        require(_priceInItheum > 0, "Price must be > 0");
+        require(_royaltyInPercent <= 100, "Royalty must be <= 100");
 
         _tokenIds.increment();
         uint256 newNFTId = _tokenIds.current();
 
         _safeMint(msg.sender, newNFTId);
         
-        dataNFTs[newNFTId] = DataNFT(newNFTId, priceInItheum, msg.sender, royaltyInPercent, true, uri);
+        _dataNFTs[newNFTId] = DataNFT(_priceInItheum, msg.sender, _royaltyInPercent, true, _uri);
         approve(address(this), newNFTId);
         
         return newNFTId;
     }
 
-    function setDataNFTPrice(uint256 tokenId, uint256 priceInItheum) public returns (bool) {
-        require(priceInItheum > 0, "Price must be > 0");
-        require(ownerOf(tokenId) == msg.sender, "Only owner can set the price");
+    function setDataNFTPrice(uint256 _tokenId, uint256 _priceInItheum) public returns (bool) {
+        require(_priceInItheum > 0, "Price must be > 0");
+        require(ownerOf(_tokenId) == msg.sender, "Only owner can set the price");
 
-        dataNFTs[tokenId].priceInItheum = priceInItheum;
+        _dataNFTs[_tokenId].priceInItheum = _priceInItheum;
 
         return true;
     }
 
-    function setDataNFTTransferable(uint256 tokenId, bool transferable) public returns (bool) {
-        require(ownerOf(tokenId) == msg.sender, "Only owner can set transferable");
+    function setDataNFTTransferable(uint256 _tokenId, bool _transferable) public returns (bool) {
+        require(ownerOf(_tokenId) == msg.sender, "Only owner can set transferable");
 
-        dataNFTs[tokenId].transferable = transferable;
+        _dataNFTs[_tokenId].transferable = _transferable;
 
         // in case transferable is false, reset approval by setting to address zero
-        address addressToApprove = transferable ? address(this) : address(0);
-        approve(addressToApprove, tokenId);
+        address addressToApprove = _transferable ? address(this) : address(0);
+        approve(addressToApprove, _tokenId);
 
         return true;
     }
 
-    function buyDataNFT(uint tokenId) public returns (bool) {
-        require(_exists(tokenId), "DataNFT doesn't exist");
+    function buyDataNFT(uint _tokenId) public returns (bool) {
+        require(_exists(_tokenId), "DataNFT doesn't exist");
 
-        require(dataNFTs[tokenId].transferable, "DataNFT is currently not transferable");
-        require(getApproved(tokenId) == address(this), "DataNFT contract must be approved to transfer the NFT");
+        require(_dataNFTs[_tokenId].transferable, "DataNFT is currently not transferable");
+        require(getApproved(_tokenId) == address(this), "DataNFT contract must be approved to transfer the NFT");
 
-        uint256 priceInItheum = dataNFTs[tokenId].priceInItheum;
-        uint256 royaltyInItheum = priceInItheum * dataNFTs[tokenId].royaltyInPercent / 100;
+        uint256 priceInItheum = _dataNFTs[_tokenId].priceInItheum;
+        uint256 royaltyInItheum = priceInItheum * _dataNFTs[_tokenId].royaltyInPercent / 100;
 
         // get and check the allowance of $ITHEUM
         uint256 allowance = itheumToken.allowance(msg.sender, address(this));
         require(allowance >= priceInItheum + royaltyInItheum, "Allowance in ITHEUM contract is too low");
 
         // transfer $ITHEUM to owner and creator
-        itheumToken.transferFrom(msg.sender, ownerOf(tokenId), priceInItheum);
-        itheumToken.transferFrom(msg.sender, dataNFTs[tokenId].creator, royaltyInItheum);
+        itheumToken.transferFrom(msg.sender, ownerOf(_tokenId), priceInItheum);
+        itheumToken.transferFrom(msg.sender, _dataNFTs[_tokenId].creator, royaltyInItheum);
 
         // transfer ownership of NFT
-        ItheumDataNFT(this).transferFrom(ownerOf(tokenId), msg.sender, tokenId);
+        ItheumDataNFT(this).transferFrom(ownerOf(_tokenId), msg.sender, _tokenId);
 
         // reset transferable
-        setDataNFTTransferable(tokenId, false);
+        setDataNFTTransferable(_tokenId, false);
 
         return true;
     }
     
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "ERC721URIStorage: URI query for nonexistent token");
 
-        return dataNFTs[tokenId].uri;
+        return _dataNFTs[_tokenId].uri;
     }
 }
