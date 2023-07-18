fa5e5a7f1f801db5fa23e1078ab9cfb306c08f68
Mark Paul <paul.mark@gmail.com>
2023-07-18 21:24:57 +1000

    smart contracts v2 deployed to astar network and docs updated

--- scripts/itheumcore-script-v2.js
+++ scripts/itheumcore-script-v2.js
@@ -13,71 +13,92 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  //// DEPLOYED for Astar + Matic
-  // console.log('process.env.ASTAR_SHIBUYA_TESTNET_URL');
-  // console.log(process.env.ASTAR_SHIBUYA_TESTNET_URL);
+  // 1) Token deployment
+  const ItheumToken = await hre.ethers.getContractFactory(
+    "ItheumTokenMultiChainGeneric"
+  );
+  const itheumToken = await ItheumToken.deploy(
+    "Itheum Protocol Astar Network Token",
+    "aITHEUM"
+  );
 
-  // const ItheumToken = await hre.ethers.getContractFactory("ItheumTokenMultiChainGeneric");
-  // const itheumToken = await ItheumToken.deploy("Itheum Protocol Astar Network Token", "aITHEUM");
+  await itheumToken.deployed();
 
-  // await itheumToken.deployed();
+  console.log("ItheumToken deployed to:", itheumToken.address);
 
-  // console.log("ItheumToken deployed to:", itheumToken.address);
-
-
-
-  // const DataDexReleaseV2 = await hre.ethers.getContractFactory("DataDexReleaseV2");
-  // const dataDEX = await DataDexReleaseV2.deploy(itheumToken.address);
-
-  // await dataDEX.deployed();
-  
-
+  /*
+  // 2) Data Dex deployment (legacy for reference)
   const DataDexReleaseV2 = await hre.ethers.getContractFactory("DataDexReleaseV2");
-  const dataDEX = await hre.upgrades.deployProxy(DataDexReleaseV2, ['0x91ff16CDfeF176b1576E640422C5BA281A242400'], {
-    initializer: "initialize",
-  });
+  const dataDEX = await DataDexReleaseV2.deploy(itheumToken.address);
+
+  await dataDEX.deployed();
+  */
+
+  // 2) Data Dex deployment
+  const DataDexReleaseV2 = await hre.ethers.getContractFactory(
+    "DataDexReleaseV2"
+  );
+  const dataDEX = await hre.upgrades.deployProxy(
+    DataDexReleaseV2,
+    [itheumToken.address],
+    {
+      initializer: "initialize",
+    }
+  );
+  // const dataDEX = await hre.upgrades.deployProxy(DataDexReleaseV2, ['0x91ff16CDfeF176b1576E640422C5BA281A242400'], {
+  //   initializer: "initialize",
+  // });
   dataDexAddress = (await dataDEX.deployed()).address;
 
   console.log("DataDEX deployed to:", dataDexAddress);
 
-
-
-
-  // const DataNFTReleaseV2 = await hre.ethers.getContractFactory("DataNFTReleaseV2");
-  // const dataNFTToken = await DataNFTReleaseV2.deploy();
-
-  // await dataNFTToken.deployed();
-
-  // console.log("DataNFT Token deployed to:", dataNFTToken.address);
-
-
+  /*
+  // 3) Data NFT deployment (legacy for reference)
   const DataNFTReleaseV2 = await hre.ethers.getContractFactory("DataNFTReleaseV2");
-  const dataNFTToken = await hre.upgrades.deployProxy(DataNFTReleaseV2, [dataDexAddress, "Itheum Data NFT-FT V1", "DATANFTFT1"], {
-    initializer: "initialize",
-  });
+  const dataNFTToken = await DataNFTReleaseV2.deploy();
+
+  await dataNFTToken.deployed();
+
+  console.log("DataNFT Token deployed to:", dataNFTToken.address);
+  */
+
+  // 3) Data NFT deployment
+  const DataNFTReleaseV2 = await hre.ethers.getContractFactory(
+    "DataNFTReleaseV2"
+  );
+  const dataNFTToken = await hre.upgrades.deployProxy(
+    DataNFTReleaseV2,
+    [dataDexAddress, "Itheum Data NFT-FT V1", "DATANFTFT1"],
+    {
+      initializer: "initialize",
+    }
+  );
   dataNFTAddress = (await dataNFTToken.deployed()).address;
 
   console.log("DataNFT token deployed to:", dataNFTAddress);
 
+  // 4) Claims Portal: DID NOT TEST THIS FOR ASTAR NETWORK (if it fails, then comment it out and fix any issues with ClaimsReleaseV2)
+  const ClaimsReleaseV2 = await hre.ethers.getContractFactory(
+    "ClaimsReleaseV2"
+  );
+  const claims = await hre.upgrades.deployProxy(
+    ClaimsReleaseV2,
+    [itheumToken.address],
+    {
+      initializer: "initialize",
+    }
+  );
 
+  await claims.deployed();
 
-  //// NOT NEEDED for Astar + Matic
-  // const Claims = await hre.ethers.getContractFactory("Claims");
-  // const claims = await hre.upgrades.deployProxy(Claims, [itheumToken.address], {
-  //   initializer: 'initialize',
-  // });
+  console.log("Claims deployed to:", claims.address);
 
-  // await claims.deployed();
-
-  // console.log("Claims deployed to:", claims.address);
-
-  // console.log(`
-  // export const itheumTokenContractAddress = '${itheumToken.address}';
-  // export const ddexContractAddress = '${dataDEX.address}';
-  // export const dNFTContractAddress = '${dataNFTToken.address}';
-  // export const claimsContractAddress = '${claims.address}';
-  // `)
-
+  console.log(`
+  export const itheumTokenContractAddress = '${itheumToken.address}';
+  export const ddexContractAddress = '${dataDEX.address}';
+  export const dNFTContractAddress = '${dataNFTToken.address}';
+  export const claimsContractAddress = '${claims.address}';
+  `);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
